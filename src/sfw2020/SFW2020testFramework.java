7ee7d93177aa152c7ab7b2f57aed30f654663b15
Mahfuj Tuhin <Mahfuj Tuhin@DESKTOP-1B5SPL0>
2020-06-07 13:38:18 -0400

    practice

--- src/sfw2020/SFW2020testFramework.java
+++ src/sfw2020/SFW2020testFramework.java
@@ -1,10 +1,15 @@
 package sfw2020;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class SFW2020testFramework {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
+		
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://www.dsebd.org/");
+		driver.manage().window().maximize();
+			}
 
 }
