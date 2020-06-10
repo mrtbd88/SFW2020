6ff032cad4dd3b983e4010399686d1254d9e9a75
Mahfuj Tuhin <Mahfuj Tuhin@MAHFUJ-1B5SPLO>
2020-06-09 22:15:35 -0400

    Test

--- src/sfw2020/SFW2020testFramework.java
+++ src/sfw2020/SFW2020testFramework.java
@@ -1,15 +1,11 @@
 package sfw2020;
 
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-
 public class SFW2020testFramework {
 
 	public static void main(String[] args) {
 		
-		WebDriver driver=new ChromeDriver();
-		driver.get("https://www.dsebd.org/");
-		driver.manage().window().maximize();
-			}
+		
+		
+	}
 
 }
